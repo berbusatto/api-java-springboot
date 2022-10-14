7c076a20a31620ff955143b5441cbfe3acfaf7b2
Bernardo Busatto <berbusatto@gmail.com>
2022-10-14 13:26:24 -0300

    api com os 4 métodos funcionando

---
+++ src/main/java/com/berbusatto/crudjavaspring/tarefas/api/TarefasAPI.java
@@ -0,0 +1,47 @@
+package com.berbusatto.crudjavaspring.tarefas.api;
+
+import com.berbusatto.crudjavaspring.tarefas.dto.TarefaDTO;
+import com.berbusatto.crudjavaspring.tarefas.facade.TarefasFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/tarefas", produces = MediaType.APPLICATION_JSON_VALUE)
+public class TarefasAPI {
+
+    @Autowired // injetando instancia da facade
+    private TarefasFacade tarefasFacade;
+
+    @PostMapping
+    @ResponseBody
+    public TarefaDTO criar(@RequestBody TarefaDTO tarefaDTO){
+        return tarefasFacade.criar(tarefaDTO);
+
+    }
+
+    @PutMapping("/{tarefaId}")
+    @ResponseBody
+    public TarefaDTO atualizar(@PathVariable("tarefaId") Long tarefaId, @RequestBody TarefaDTO tarefaDTO){
+        return tarefasFacade.atualizar(tarefaDTO, tarefaId);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public List<TarefaDTO> getAll(){
+        return tarefasFacade.getAll();
+    }
+
+    @DeleteMapping("/{tarefaId}")
+    @ResponseBody
+    public String deletar(@PathVariable("tarefaId") Long tarefaId){
+        return tarefasFacade.delete(tarefaId);
+    }
+
+
+
+}
